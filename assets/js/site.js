7e33789475e7d967862534cad319958dcf1bce79
Orugga-dev <jose.ugarte@orugga.com>
2026-01-17 11:09:50 -0300

    Delete assets directory

--- assets/js/site.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  const btn = document.querySelector('[data-mobile-toggle]');
-  const panel = document.querySelector('[data-mobile-panel]');
-  if(!btn || !panel) return;
-  btn.addEventListener('click', () => {
-    const open = panel.classList.toggle('open');
-    btn.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-})();
